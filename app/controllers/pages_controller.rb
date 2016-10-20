d5a3282684e2a373f672133399ff2938c841cefc
rkcudjoe <rkcudjoe@gmail.com>
2016-10-20 11:19:52 -0400

    change dates for screenshots

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -31,7 +31,7 @@ class PagesController < ApplicationController
   def screenshots
     if current_user.hubstaff_auth_token.present?
       HUBSTAFF_CLIENT.auth_token = current_user.hubstaff_auth_token
-      @hubstaff_screenshots = HUBSTAFF_CLIENT.screenshots("2016-05-22","2016-05-24", projects: "112761")
+      @hubstaff_screenshots = HUBSTAFF_CLIENT.screenshots("2016-09-29","2016-10-01", projects: "112761")
       render :screenshots
     else
       render :integration, alert: "Please Connect To Hubstaff"
@@ -41,7 +41,7 @@ class PagesController < ApplicationController
   def activities
     if current_user.hubstaff_auth_token.present?
       HUBSTAFF_CLIENT.auth_token = current_user.hubstaff_auth_token
-      @hubstaff_activities = HUBSTAFF_CLIENT.activities("2016-05-22","2016-05-24", users: "61188")
+      @hubstaff_activities = HUBSTAFF_CLIENT.activities("2016-09-29","2016-10-01", users: "61188")
       render :activities
     else
       render :integration, alert: "Please Connect To Hubstaff"
